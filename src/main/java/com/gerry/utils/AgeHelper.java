306e07d6c71fb309e6ac607c0c7ac8bbf75923b2
Gerry Baird <gerry.baird@uk.ibm.com>
2022-03-08 16:23:12 +0000

    Added pure function and switch to LocalDate for AgeHelper.getAge()

--- src/main/java/com/gerry/utils/AgeHelper.java
+++ src/main/java/com/gerry/utils/AgeHelper.java
@@ -1,25 +1,21 @@
 package com.gerry.utils;
 
+import com.ibm.rules.engine.annotations.PureFunction;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 public class AgeHelper {
 
-    public static int getAge(Date now, Date dateOfBirth) {
-        LocalDate currentDate = convertToLocalDateViaSqlDate(now);
-        LocalDate birthDate = convertToLocalDateViaSqlDate(dateOfBirth);
+    @PureFunction
+    public static int getAge(LocalDate now, LocalDate dateOfBirth) {
 
-        if ((birthDate != null) && (currentDate != null)) {
-            //return Period.between(birthDate, currentDate).getYears();
-            long years = ChronoUnit.YEARS.between(birthDate, currentDate);
+        if ((dateOfBirth != null) && (now != null)) {
+            long years = ChronoUnit.YEARS.between(dateOfBirth, now);
             return (int)years;
         } else {
             return 0;
         }
-    }
-
-    private static LocalDate convertToLocalDateViaSqlDate(Date dateToConvert) {
-        return new java.sql.Date(dateToConvert.getTime()).toLocalDate();
     }
 }
--- src/test/java/com/gerry/utils/AgeHelperTest.java
+++ src/test/java/com/gerry/utils/AgeHelperTest.java
@@ -1,35 +1,35 @@
 package com.gerry.utils;
 
 import org.junit.jupiter.api.Test;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AgeHelperTest {
     @Test
-    void testGetAge50() throws ParseException {
+    void testGetAge50() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         String sNow="28/06/2021";
         String sDob="15/06/1971";
 
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
-        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
+        LocalDate dob = LocalDate.parse(sDob, formatter);
+        LocalDate now = LocalDate.parse(sNow, formatter);
 
         int age = AgeHelper.getAge(now, dob);
 
         assertEquals(50, age);
     }
     @Test
-    void testGetAge5() throws ParseException {
-
+    void testGetAge5() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String sNow="01/05/2021";
         String sDob="15/06/2015";
 
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
-        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
+        LocalDate dob = LocalDate.parse(sDob, formatter);
+        LocalDate now = LocalDate.parse(sNow, formatter);
 
         int age = AgeHelper.getAge(now, dob);
 
@@ -37,13 +37,13 @@ class AgeHelperTest {
     }
 
     @Test
-    void testGetAge16() throws ParseException {
-
+    void testGetAge16() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         String sNow="04/10/2021";
         String sDob="26/09/2005";
 
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(sDob);
-        Date now = new SimpleDateFormat("dd/MM/yyyy").parse(sNow);
+        LocalDate dob = LocalDate.parse(sDob, formatter);
+        LocalDate now = LocalDate.parse(sNow, formatter);
 
         int age = AgeHelper.getAge(now, dob);
 
